8950bc14bfa0f4fcca91df6f3f1be7e206050365
Sean Blessing <snblessing@gmail.com>
2025-02-04 16:22:44 -0500

    ch6 demos and ex-dice-roller

---
+++ Chap06Demos/Chap06Demos/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Chap06Demos {
+    internal class Program {
+        static void Main(string[] args) {
+            Print("Welcome to Chapter 6 Demos - Methods!!");
+            // Get info for a movie (title, year, rating, director)
+            string choice = "y";
+            while (choice == "y") {
+                string title = GetString("Enter movie title: ");
+                int year = GetInt("Enter release year: ", 1900, 2050);
+                string rating = GetString("Enter age rating: ");
+                string director = GetString("Enter director: ");
+
+                // validate y or n
+                choice = GetString("Continue? (y/n): ", "y", "n");
+            }
+            Print("bye");
+        }
+
+        static int GetInt(string prompt) {
+            Print(prompt, "");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static int GetInt(string prompt, int min, int max) {
+            bool success = false;
+            int nbr = 0;
+            while (!success) {
+                Print(prompt, "");
+                nbr = Int32.Parse(Console.ReadLine());
+                if (nbr < min || nbr > max) {
+                    Print("Error: out of range. Try again.");
+                }
+                else {
+                    success = true;
+                }
+            }
+            return nbr;
+        }
+
+        static string GetString(string prompt, string str1, string str2) {
+            string retStr = "";
+            bool success = false;
+            while (!success) {
+                retStr = GetString(prompt);
+                if (retStr.ToLower() == str1 || retStr.ToLower() == str2) {
+                    success = true;
+                }
+                else {
+                    Print("Error: Entry must be either '" + str1 + "' or '" + str2 + "'. Try again.");
+                }
+            }
+            return retStr;
+        }
+
+        // GetString method: accepts a prompt: string, returns a string from the console
+        static string GetString(string prompt) {
+            Print(prompt, "");
+            return Console.ReadLine();
+        }
+
+        // Method accepts one input variable, returns void (no return value)
+        static void Print(string msg, string endLine="\n") {
+            Console.Write(msg + endLine);
+        }
+    }
+}
---
+++ Chap06ExDiceRoller/Chap06ExDiceRoller/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Chap06ExDiceRoller {
+    internal class Program {
+        static void Main(string[] args) {
+            Print("Dice Roller!");
+            Print("");
+
+            string choice = PromptChoice("Roll the dice ? ");
+            Print("");
+            Random rand = new Random();
+            while (choice == "y") {
+                int die1 = RollDie(rand);
+                int die2 = RollDie(rand);
+                int total = die1 + die2;
+                DisplayResults(die1, die2, total);
+                Print("");
+                choice = PromptChoice("Roll again? (y/n): ");
+                Print("");
+            }
+            Print("Bye");
+        }
+
+        private static string PromptChoice(string prompt) {
+            Console.Write(prompt);
+            return Console.ReadLine().ToLower();
+        }
+
+        private static void DisplayResults(int die1, int die2, int total) {
+            Print("Die 1: " + die1);
+            Print("Die 2: " + die2);
+            Print("Total: " + total);
+            if (total == 2) {
+                Print("Snake eyes!");
+            }
+            else if (total == 12) {
+                Print("Box cars!");
+            }
+        }
+
+        private static int RollDie(Random rand) {
+            return rand.Next(1, 7);
+        }
+
+        private static void Print(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
