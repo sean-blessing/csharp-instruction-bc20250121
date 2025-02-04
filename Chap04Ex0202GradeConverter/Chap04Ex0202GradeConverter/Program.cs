2a94f2d0873d597c5f23c6e02f5297e8ccacd116
Sean Blessing <snblessing@gmail.com>
2025-02-03 19:09:28 -0500

    Chapter 4 Exercise Demos

---
+++ Chap04Ex0202GradeConverter/Chap04Ex0202GradeConverter/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Chap04Ex0202GradeConverter {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Grade Converter App!");
+
+            String choice = "y";
+            // upper or lowercase y is ok
+            //while (choice == "y" || choice == "Y") {
+            while (choice.ToLower() == "y") {
+                // prompt for input - numericGrade: int
+                Console.Write("Enter numeric grade: ");
+                //int numericGrade = Convert.ToInt32(Console.ReadLine());
+                int numericGrade = Int32.Parse(Console.ReadLine());
+                // do business logic
+                String letterGrade = "";
+                if (numericGrade >= 88)
+                    letterGrade = "A";
+                else if (numericGrade >= 80)
+                    letterGrade = "B";
+                else if (numericGrade >= 68)
+                    letterGrade = "C";
+                else if (numericGrade >= 60)
+                    letterGrade = "D";
+                else
+                    letterGrade = "F";
+                // display result - display letterGrade
+                Console.WriteLine("Letter grade: " + letterGrade);
+
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ Chap04Ex0203RectangleCalc/Chap04Ex0203RectangleCalc/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Chap04Ex0203RectangleCalc {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Rectangle Calculator App!");
+
+            // get input - length: double, width: double
+            Console.Write("Enter length: ");
+            decimal length = Decimal.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            decimal width = Decimal.Parse(Console.ReadLine());
+
+            // do biz logic
+            // - calc area: length * width
+            // - calc perimeter: 2 * length + 2 * width
+            decimal area = length * width;
+            decimal perimeter = 2 * length + 2 * width;
+
+            // display output
+            // - output area and perimeter
+            Console.WriteLine("Area:\t\t" + area);
+            Console.WriteLine("Perimeter:\t" + perimeter);
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
