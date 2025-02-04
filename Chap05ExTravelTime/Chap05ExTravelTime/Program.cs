f1cc7048d8b8d0eb265d8b376945ccf519ca96ab
Sean Blessing <snblessing@gmail.com>
2025-02-04 16:22:09 -0500

    chap 5 exs

---
+++ Chap05ExTravelTime/Chap05ExTravelTime/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Chap05ExTravelTime {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Travel Time Calculator app!!");
+
+            string choice = "y";
+
+            while (choice == "y") {
+                //get input: miles, mph
+                Console.WriteLine("Enter miles: ");
+                double miles = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter mph: ");
+                double mph = Double.Parse(Console.ReadLine());
+
+                // biz logic: hours and minutes (whole numbers)
+                // totaltime: double = miles/mph
+                // hours: int = (int)totaltime
+                // minutes: int = (int)((miles % mph)*60)
+                double totalTime = miles / mph;
+                int hours = (int)totalTime;
+                double remainder = totalTime - hours;
+                int minutes = (int)((remainder) * 60);
+
+                // display output
+                Console.WriteLine("Hours:    " + hours);
+                Console.WriteLine("Minutes:  " + minutes);
+
+
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("bye");
+        }
+    }
+}
---
+++ Chap5ExChangeCalc/Chap5ExChangeCalc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace Chap5ExChangeCalc {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the change calculator!");
+            
+            string choice = "y";
+            while (choice == "y") {
+                // get input - cents: int
+                Console.Write("\nEnter number of cents (0-99): ");
+                int cents = Int32.Parse(Console.ReadLine());
+                // biz logic:
+                // - quarters: int = cents / 25
+                int quarters = cents / 25;
+                // - cents = cents % 25
+                cents %= 25;
+                // - dimes: int = cents / 10
+                int dimes = cents / 10;
+                // - cents = cents % 10
+                cents %= 10;
+                // - nickels: int = cents / 5
+                int nickels = cents / 5;
+                // - pennies: int = cents % 5
+                int pennies = cents % 5;
+                Console.WriteLine();
+                Console.WriteLine("Quarters: " + quarters);
+                Console.WriteLine("Dimes:    " + dimes);
+                Console.WriteLine("Nickels:  " + nickels);
+                Console.WriteLine("Pennies:  " + pennies);
+                Console.WriteLine();
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine();
+            }
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
