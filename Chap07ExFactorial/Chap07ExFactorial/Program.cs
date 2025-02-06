2d0f0dcf2129ea3a33ffa51176a158394fb27c9b
Sean Blessing <snblessing@gmail.com>
2025-02-06 14:20:06 -0500

    ch7 project demos

---
+++ Chap07ExFactorial/Chap07ExFactorial/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Chap07ExFactorial {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Factorial App!");
+
+            string choice = "y";
+            while (choice == "y") {
+                // get input theNumber: long
+                //long theNumber = GetLong("Enter an integer that's greater than 0 and less than 1000: ", 1, 1000);
+                //// compute the factorial w/ for loop - factorial: long = 1
+                //// for i = 1, i <= theNumber, i++ => factorial = factorial * i
+                //long theFactorial = 1;
+                //for (int i = 1; i <= theNumber; i++) {
+                //    //theFactorial = theFactorial * i;
+                //    theFactorial *= i;
+                //}
+                //Console.WriteLine("The factorial of " + theNumber + " is " + theFactorial + ".");
+
+                long theFactorial = 1;
+                for (int i = 1; i <= 30; i++) {
+                    theFactorial *= i;
+                    Console.WriteLine("i: " + i + ", factorial: " + theFactorial);
+                }
+                Console.Write("Continue? (y/n) ");
+                choice = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("Bye");
+        }
+
+        private static long GetLong(string prompt, long min, long max) {
+            long result = 0;
+            bool isValid = false;
+            while (!isValid) {
+                Console.Write(prompt);
+                if (!long.TryParse(Console.ReadLine(), out result)) {
+                    Console.WriteLine("Error - invalid whole number. Try again.");
+                    continue;
+                }
+                if (result < min || result > max) {
+                    Console.WriteLine("Error - input must be within range (" + min + "-" + max + "). Try again.");
+                }
+                else {
+                    isValid = true;
+                }
+            }
+
+            return result;
+        
+        }
+    }
+}
---
+++ Chap07ExTipCalculator/Chap07ExTipCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Chap07ExTipCalculator {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Tip Calculator!");
+
+            string choice = "y";
+            while (choice == "y") {
+                Console.Write("Cost of meal: ");
+                // skipping validation - assume user enters valid cost
+                decimal costOfMeal = Decimal.Parse(Console.ReadLine());
+                // biz logic:
+                // - calculate and display tipAmt: decimal and totalAmt: decimal for .15, .20 and .25
+                // for decimal d = .15m; d <= .25m; d+=.05
+                for (decimal d = .15m; d <= .25m; d+=.05m) {
+                    decimal tipAmt = costOfMeal * d;
+                    decimal totalAmt = costOfMeal + tipAmt;
+                    Console.WriteLine(d.ToString("p0"));
+                    Console.WriteLine("Tip Amount: "+tipAmt.ToString("c"));
+                    Console.WriteLine("Total Amount: "+totalAmt.ToString("c"));
+                    Console.WriteLine();
+                }
+
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            }
+
+
+
+            Console.WriteLine("bye");
+        }
+    }
+}
