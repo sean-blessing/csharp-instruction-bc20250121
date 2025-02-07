941166f73a035d9020b5ccc19aff23f06ed6a7ef
Sean Blessing <snblessing@gmail.com>
2025-02-07 15:05:38 -0500

    quick demo on lists and indexed for loop, accessing element by index position

---
+++ Chap08ListsGetByIndex/Chap08ListsGetByIndex/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Chap08ListsGetByIndex {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to a supplemental demo on Lists!");
+            List<int> list = new List<int> { 2, 5, 20, 3, 7, 9 };
+            foreach (int i in list) {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < list.Count; i++) {
+                Console.WriteLine($"i: {i}, {list[i]}");
+            }
+
+            Console.WriteLine($"element at index 2: {list[2] }");
+
+            Console.WriteLine("bye");
+        }
+    }
+}
