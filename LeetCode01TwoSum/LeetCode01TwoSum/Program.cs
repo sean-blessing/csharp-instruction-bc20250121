f464ae22d92de678acf42746dfbc0e98c2c90e70
Sean Blessing <snblessing@gmail.com>
2025-02-07 16:01:23 -0500

    leet code TwoSum setup

---
+++ LeetCode01TwoSum/LeetCode01TwoSum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode01TwoSum {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Two Sum Testing!");
+
+            // example 1
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] indices = TwoSum(nums, target);
+
+            Console.WriteLine("indices: ");
+            foreach (int i in indices) {
+                Console.WriteLine($"indices: {indices[i]}");
+            }
+
+
+            Console.WriteLine("bye");
+        }
+
+        static int[] TwoSum(int[] nums, int target) {
+            int[] indices = new int[2];
+            // write the method!
+
+            return indices;
+        }
+    }
+}
