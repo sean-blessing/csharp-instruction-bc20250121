992134559b6e198ecea9bbbddbc66a605c60845f
Sean Blessing <snblessing@gmail.com>
2025-02-10 13:22:56 -0500

    Two Sum

--- LeetCode01TwoSum/LeetCode01TwoSum/Program.cs
+++ LeetCode01TwoSum/LeetCode01TwoSum/Program.cs
@@ -3,25 +3,52 @@
         static void Main(string[] args) {
             Console.WriteLine("Two Sum Testing!");
 
-            // example 1
+            Console.WriteLine("---- Example 1 ----");
             int[] nums = { 2, 7, 11, 15 };
             int target = 9;
-
             int[] indices = TwoSum(nums, target);
+            DisplayResults(indices);
+            Console.WriteLine();
 
-            Console.WriteLine("indices: ");
-            foreach (int i in indices) {
-                Console.WriteLine($"indices: {indices[i]}");
-            }
+            Console.WriteLine("---- Example 2 ----");
+            nums = new int[] { 3, 2, 4 };
+            target = 6;
+            indices = TwoSum(nums, target);
+            DisplayResults(indices);
+            Console.WriteLine();
 
+            Console.WriteLine("---- Example 3 ----");
+            nums = new int[] { 3,3 };
+            target = 6;
+            indices = TwoSum(nums, target);
+            DisplayResults(indices);
+            Console.WriteLine();
 
             Console.WriteLine("bye");
         }
 
-        static int[] TwoSum(int[] nums, int target) {
-            int[] indices = new int[2];
-            // write the method!
+        static void DisplayResults(int[] indices) {
+            Console.WriteLine("indices: ");
+            foreach (int i in indices) {
+                Console.WriteLine($"indices: {i}");
+            }
+        }
 
+        static int[] TwoSum(int[] nums, int target) {
+            int[] indices = [];
+            for (int i = 0; i < nums.Length; i++) {
+                if (indices.Length > 0) {
+                    break;
+                }
+                for (int j = 0; j < nums.Length; j++) {
+                    if (j != i) {
+                        if (nums[i] + nums[j] == target) {
+                            indices = [i, j];
+                            break;
+                        }
+                    }
+                }
+            }
             return indices;
         }
     }
---
+++ LeetCodeTwoSumDemo/LeetCodeTwoSumDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCodeTwoSumDemo {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the TwoSum LeetCode Challenge!");
+
+            int[] nums = [2, 7, 11, 15];
+            // test case 1
+            int[] indices = TwoSum(nums, 9);
+            DisplayResults(indices, "Test Case 1");
+
+            nums = [3, 2, 4];
+            // test case 2
+            indices = TwoSum(nums, 6);
+            DisplayResults(indices, "Test Case 2");
+
+            nums = [1, 3, 7, 12, 15];
+            // test case 3
+            indices = TwoSum(nums, 19);
+            DisplayResults(indices, "Test Case 3");
+            Console.WriteLine("Bye");
+        }
+
+        static void DisplayResults(int[] indices, string msg) {
+            PrintLine(msg);
+            foreach (int i in indices) {
+                Print($"index: {i}, ");
+            }
+            PrintLine("");
+        }
+
+        static int[] TwoSum(int[] nums, int target) {
+            int[] indices = new int[2];
+            // nums - array of ints, one pair of numbers, summed, equals target
+            // return indices of those two numbers
+            // my first #
+            bool found = false;
+            for (int i = 0; i < nums.Length; i++) {
+                if (found) {
+                    break;
+                }
+                // my second #
+                for (int j = 0; j < nums.Length; j++) {
+                    if (i != j) {
+                        Print($"{nums[i]} + {nums[j]}, ");
+                        int sum = nums[i] + nums[j];
+                        if (sum == target) {
+                            Print("Target!!!");
+                            indices = [i, j];
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                PrintLine("");
+            }
+
+            return indices;
+        }
+
+        static void Print(string msg) {
+            Console.Write(msg);
+        }
+
+        static void PrintLine(string msg) {
+            Console.WriteLine(msg);
+        }
+    }
+}
