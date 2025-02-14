9f3f8738469464d88cd7329f870977b8cc7b4eea
Sean Blessing <snblessing@gmail.com>
2025-02-14 11:29:31 -0500

    added getdouble and getdecimal

--- ConsoleUtil/ConsoleUtil/MyConsole.cs
+++ ConsoleUtil/ConsoleUtil/MyConsole.cs
@@ -34,9 +34,35 @@
 
         public static double PromptDouble(string prompt) {
             double result = 0;
+            bool isValid = false;
+            while (!isValid) {
+                Print(prompt);
+                if (Double.TryParse(Console.ReadLine(), out result)) {
+                    isValid = true;
+                }
+                else {
+                    PrintLine("Error - invalid double.");
+                }
+            }
+
             return result;
         }
 
+        public static decimal PromptDecimal(string prompt) {
+            decimal result = 0m;
+            bool isValid = false;
+            while (!isValid) {
+                Print(prompt);
+                if (Decimal.TryParse(Console.ReadLine(), out result)) {
+                    isValid = true;
+                }
+                else {
+                    PrintLine("Error - invalid decimal.");
+                }
+            }
+
+            return result;
+        }
         public static string PromptString(string prompt) {
             Print(prompt);
             return Console.ReadLine();
