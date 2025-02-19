b81b8873b24a9d59b086d009f6346c1a14ddfe90
Sean Blessing <snblessing@gmail.com>
2025-02-19 14:06:01 -0500

    hangman / substring / indexof demos

---
+++ Chap09DemoHangmanHiddenWordStuff/Chap09DemoHangmanHiddenWordStuff/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleUtil;
+
+namespace Chap09DemoHangmanHiddenWordStuff {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Hangman HiddenWord Resolution.\n");
+
+            // define theWord
+            string theWord = "superstitious";
+            MyConsole.PrintLine($"theWord = {theWord}");
+
+            // generate hiddenWord (string == theWord.Length, all underscores)
+            string hiddenWord = "";
+            // # letters in theWord
+            // hiddenWord -> same length as theWord, all underscores
+            foreach (char word in theWord) {
+                hiddenWord += "_";
+            }
+            MyConsole.PrintLine($"hiddenWord: {hiddenWord}");
+
+            DisplayHiddenWord(hiddenWord);
+
+            string letter = "";
+            bool winner = false;
+            while (!winner) {
+                // resolve correctly guessed letter
+                // prompt user for a letter: string
+                letter = MyConsole.PromptString("Guess a letter: ");
+                // iterate over theWord, and find any matches for letter
+                char[] charsTheWord = theWord.ToCharArray();
+                char[] charsHiddenWord = hiddenWord.ToCharArray();
+                for (int i = 0; i < charsTheWord.Length; i++) {
+                    char c = charsTheWord[i];
+                    if (letter.Equals(c.ToString())) {
+                        MyConsole.PrintLine($"Letter found! {i}");
+                        // resolve this character in hiddenWord
+                        charsHiddenWord[i] = c;
+                    }
+                }
+                hiddenWord = new string(charsHiddenWord);
+                // if (hiddenWord == theWord) {
+                if (hiddenWord.IndexOf('_') == -1) {
+                    winner = true;
+                }
+                DisplayHiddenWord(hiddenWord);
+            }
+            MyConsole.PrintLine("\nBye");
+        }
+
+        private static void DisplayHiddenWord(string hiddenWord) {
+            // display hiddenWord w/ spaces separating the underscores
+            MyConsole.PrintLine("display hiddenWord");
+            foreach (char c in hiddenWord) {
+                MyConsole.Print(c + " ");
+            }
+            MyConsole.PrintLine();
+        }
+    }
+}
---
+++ Chap09DemosSubstringIndex/Chap09DemosSubstringIndex/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleUtil;
+
+namespace Chap09DemosSubstringIndex {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to index and substring");
+
+            string letters = "abcdefg";
+            string first3Letters = letters.Substring(0,3);
+            MyConsole.PrintLine($"first 3 letters: {first3Letters}");
+            MyConsole.PrintLine($"next 3 letters: {letters.Substring(3,3)}");
+            MyConsole.PrintLine($"last character: {letters.Substring(6, 1)}");
+
+            string fullName = "Robert Nesta Marley";
+            // first, middle, and last names
+            // where the spaces are
+            int firstSpace = fullName.IndexOf(" ");
+            MyConsole.PrintLine($"firstSpace = {firstSpace}"); // 6
+            int secondSpace = fullName.IndexOf(" ",firstSpace+1);
+            MyConsole.PrintLine($"secondSpace = {secondSpace}"); //12
+
+            string firstName = fullName.Substring(0, firstSpace);
+            MyConsole.PrintLine($"firstName = {firstName}.");
+            string middleName = fullName.Substring(firstSpace+1, (secondSpace - firstSpace - 1));
+            MyConsole.PrintLine($"middleName = {middleName}.");
+            string lastName = fullName.Substring(secondSpace+1);
+            MyConsole.PrintLine($"middleName = {lastName}.");
+
+            MyConsole.PrintLine($"x position = {fullName.IndexOf('x')}");
+
+
+
+        }
+    }
+}
