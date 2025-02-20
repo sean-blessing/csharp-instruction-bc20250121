2d3f93f3cc82f4b1f34d82c310120308d2892bd7
Sean Blessing <snblessing@gmail.com>
2025-02-20 16:47:16 -0500

    web api demo

---
+++ OurFirstWebAPIProject/OurFirstWebAPIProject/Controllers/SimpleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebAPIProject.Controllers {
+    [Route("api/hello-world")]
+    [ApiController]
+    public class SimpleController : ControllerBase {
+        private static List<string> names = new List<string>();
+
+        // p. 51 - Manually add a controller
+        [HttpGet("a")]
+        public string HelloWorld() {
+            return "Hello World!!!";
+        }
+
+        [HttpGet("b")]
+        public string HelloWorld2() {
+            return "abc";
+        }
+
+        // see bottom p. 43
+        [HttpGet("say-hello/{message}")]
+        public string SayHello(string message) {
+            return $"Hello {message}";
+        }
+
+        [HttpGet("add-name/{name}")]
+        public string AddName(string name) {
+            names.Add(name);
+            return $"{name} added";
+        }
+        [HttpGet("names")]
+        public string GetNames() {
+            string namesStr = string.Join("|", names);
+            return namesStr;
+        }
+    }
+}
