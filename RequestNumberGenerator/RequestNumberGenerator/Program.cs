d69f8623bd754111df1049df1f25c255272927fd
Sean Blessing <snblessing@gmail.com>
2025-02-25 14:11:49 -0500

    PRS RequestNumberGenerator demo

---
+++ RequestNumberGenerator/RequestNumberGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace RequestNumberGenerator {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Request Number Creator!");
+            Console.WriteLine("These #s will be based on the current date.\n");
+
+            Console.WriteLine("The format for a request # is 'R{YYMMDD####}', where '####' is the 4 digit #:");
+            Console.WriteLine("Here are 10 request #s for today: ");
+            int requestCounter = 0;
+            for (int i = 0; i < 1001; i++) {
+                StringBuilder requestNbr = new StringBuilder("R");
+                string dateStr = String.Format("{0:yyMMdd}", DateTime.Now);
+                //string yy = today.Year.ToString().Substring(2);
+                //string mm = today.Month.ToString().PadLeft(2, '0');
+                //string dd = today.Day.ToString().PadLeft(2, '0');
+                requestCounter++;
+                string count = requestCounter.ToString().PadLeft(4, '0');
+                requestNbr.Append(dateStr).Append(count);
+                Console.WriteLine($"Request[{i}]: {requestNbr}");
+            }
+
+            Console.WriteLine("Bye");
+
+        }
+    }
+}
