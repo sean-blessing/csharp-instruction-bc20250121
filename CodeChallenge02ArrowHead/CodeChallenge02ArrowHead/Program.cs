0ebe1eff8b956b5840ff7fa68563da9d50f6f642
Sean Blessing <snblessing@gmail.com>
2025-03-04 15:51:22 -0500

    arrowhead and make chocolate

---
+++ CodeChallenge02ArrowHead/CodeChallenge02ArrowHead/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeChallenge02ArrowHead {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Arrowhead Challenge\n");
+
+            string choice = "y";
+            while (choice == "y") {
+                Console.Write("Enter length of arrowhead: ");
+                int length = Int32.Parse(Console.ReadLine());
+                if (length % 2 == 0) {
+                    length++;
+                }
+                int midpoint = (int)Math.Ceiling((double)length / 2);
+                Console.WriteLine($"length: {length}, midpoint: {midpoint}");
+
+                string output = "";
+                for (int i = 1; i <= midpoint; i++) {
+                    output += ">";
+                    Console.WriteLine(output);
+                }
+                for (int i = 1; i < midpoint; i++) {
+                    output = output.Substring(0, output.Length - 1);
+                    Console.WriteLine(output);
+                }
+
+
+                Console.Write("Again? (y/n) ");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ CodingChallenge01MakeChocolate/CodingChallenge01MakeChocolate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CodingChallenge01MakeChocolate {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Let's make some chocolate!");
+
+            // test case: makeChocolate(4, 1, 9) → 4
+            Console.WriteLine($"makeChocolate(4, 1, 9): {makeChocolate(4, 1, 9)}");
+
+            // makeChocolate(4, 1, 10) → -1
+            Console.WriteLine($"makeChocolate(4, 1, 10): {makeChocolate(4, 1, 10)}");
+
+            // makeChocolate(4, 1, 7) → 2
+            Console.WriteLine($"makeChocolate(4, 1, 7): {makeChocolate(4, 1, 7)}");
+
+            Console.WriteLine("Bye");
+        }
+
+        public static int makeChocolate(int small, int big, int goal) {
+            int result = 0;
+            // goal - (big * 5) = smallKilos
+            // if (smallKilos <= small) => return smallKilos else return -1
+            int smallKilos = goal - (big * 5);
+            result = (smallKilos <= small) ? smallKilos : -1;
+
+            return result;
+        }
+    }
+}
